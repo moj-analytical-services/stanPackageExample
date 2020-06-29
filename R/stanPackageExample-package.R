af09d115b12b0c9832951014f0f89257bb331bb3
odaniel1 <owendnl@gmail.com>
2020-06-29 07:50:15 +0000

    Initial commit

---
+++ R/stanPackageExample-package.R
@@ -0,0 +1,16 @@
+#' The 'stanPackageExample' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name stanPackageExample-package
+#' @aliases stanPackageExample
+#' @useDynLib stanPackageExample, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. https://mc-stan.org
+#'
+NULL
